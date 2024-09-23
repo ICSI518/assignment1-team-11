7cbb3aac1492f0009bc2df45b786be829f7fcfa3
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2024-09-22 22:58:24 -0400

    Welcome1 Page is ready- The First Page which user will interact

--- AgroAssit/frontend/src/App.js
+++ AgroAssit/frontend/src/App.js
@@ -1,17 +1,11 @@
-import logo from './logo.svg';
-import { GoogleFrame } from './components/GoogleFrame24';
-import { EmailForm } from './components/EmailForm';
-import { PasswordForm } from './components/PasswordForm';
-import { SigninButton } from './components/SigninButton';
+import Welcome from './Welcome';
 import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <GoogleFrame onClick={() => {}} />
-      <EmailForm> </EmailForm>
-      <PasswordForm> </PasswordForm>
-      <SigninButton></SigninButton>
+      
+      <Welcome></Welcome>
 
     </div>
   );
